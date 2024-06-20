b7c552317c44a152be50ecec2e8caefeeafb3826
Nolen Johnson <johnsonnolen@gmail.com>
2024-06-20 23:45:54 +0300

    s2: Drop legacy trust HAL
    * Dead, will need to move to newer USB HAL at some
      point if we decide to.

--- device.mk
+++ device.mk
@@ -445,10 +445,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.s2
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic \
