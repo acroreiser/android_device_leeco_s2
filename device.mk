016a339ce8327b5576987c77b463c0dfa9894efe
Tommaso Fonda <fondatommaso2@gmail.com>
2024-07-23 22:29:16 +0300

    s2: Build android.hardware.radio.config@1.0-service
    Fixes lags when entering SIM settings.

--- device.mk
+++ device.mk
@@ -361,6 +361,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjson
 
+PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.0-service
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
