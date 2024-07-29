20d4d10a96596dc744e8fde1b37e7b002b6f18bf
Yaroslav Zviezda <acroreiser@gmail.com>
2024-07-29 22:37:13 +0300

    Revert "s2: Use default keymaster@4.1 HIDL"
    This reverts commit f4f317f7bfe38f12d7a03b071cd92ecb4b0494db.

--- device.mk
+++ device.mk
@@ -243,7 +243,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1-service
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Libshims
 PRODUCT_PACKAGES += \
