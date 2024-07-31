a84aa0655ff3b08f71c13cc51ed039d5a9968576
Yaroslav Zviezda <acroreiser@gmail.com>
2024-07-31 21:35:32 +0300

    [DNM] s2: allow user builds with SELINUX_IGNORE_NEVERALLOWS
    * Explicitely set SELINUX_IGNORE_NEVERALLOWS true for user builds.
    * For userdebug/eng builds SELINUX_IGNORE_NEVERALLOWS already defined in device/qcom/sepolicy-legacy.
    
    NOTE: this commit is intended for my own builds and will not work without hacks in system/sepolicy.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,11 +190,13 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-# Sepolicy
+# SELinux
+ifeq ($(TARGET_BUILD_VARIANT),user)
+	SELINUX_IGNORE_NEVERALLOWS := true
+endif
 include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
