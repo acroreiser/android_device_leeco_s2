14f8afaad5cdb2c541c0acd0be755ed8f781c6bc
Yumi Yukimura <me.cafebabe@gmail.com>
2025-01-02 00:56:44 +0300

    s2: Switch to USB 1.3-basic HAL
    Change-Id: I6f477db2220b3563b44bb9545b7889166b8a6b06

--- device.mk
+++ device.mk
@@ -481,7 +481,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.cyanogen_8916
+    android.hardware.usb@1.3-service.basic
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
