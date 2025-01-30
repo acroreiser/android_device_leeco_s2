a0c2dee44dd5990077baaf38f4a9bfa8bbc1cce2
Yaroslav Zviezda <acroreiser@gmail.com>
2025-01-30 20:04:33 +0200

    Revert "s2: ril: add rild socket shim"
    This reverts commit b05ad11f696e699d27c75cf0e21b8dcc1aee6e37.

--- device.mk
+++ device.mk
@@ -419,8 +419,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.0.vendor \
     librmnetctl \
-    libxml2 \
-    rild_socket
+    libxml2
 
 # Screen density
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
