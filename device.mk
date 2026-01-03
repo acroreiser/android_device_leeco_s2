de210a82ae02c8ac966c47411c8af35614a88163
Derek <nielson925@gmail.com>
2026-01-03 15:20:01 +0200

    s2: Use lineage radio 1.4 wrapper
    Change-Id: Id51367011e82d7d474eb9fa33d49079f1277fba0

--- device.mk
+++ device.mk
@@ -387,7 +387,8 @@ PRODUCT_PACKAGES += \
     libjson
 
 PRODUCT_PACKAGES += \
-    android.hardware.radio.config@1.0-service
+    android.hardware.radio.config@1.0-service \
+    android.hardware.radio@1.4-service.legacy
 
 # Ramdisk
 PRODUCT_PACKAGES += \
