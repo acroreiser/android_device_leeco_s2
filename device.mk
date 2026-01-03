efe12a371817b00ae68d610fdef15e238bde04fb
Bruno Martins <bgcngm@gmail.com>
2026-01-03 15:01:23 +0200

    s2: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -156,7 +156,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Fake logprint for fingerprint libs
 PRODUCT_PACKAGES += \
