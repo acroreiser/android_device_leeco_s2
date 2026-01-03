c2c340d237b45553f120d602f4b42101bb87a6bd
LuK1337 <priv.luk@gmail.com>
2026-01-03 15:32:17 +0200

    s2: Migrate to AIDL camera provider
    Change-Id: I290ecfd9760d6428fe64f137974c353846c0e35d

--- device.mk
+++ device.mk
@@ -92,8 +92,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider-service.lineage
 
 ifneq ($(USE_PROPRIETARY_CAMERA),true)
 PRODUCT_PACKAGES += \
